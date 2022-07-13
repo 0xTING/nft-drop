716f72bd45d10ef4a1ee63e0d3252c06ccdb350a
jason <jason_052@hotmail.com>
2022-07-13 22:47:26 +0100

    chain to rink

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,13 @@ import "../styles/globals.css";
 import Head from "next/head";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mumbai;
+const activeChainId = ChainId.Rinkeby;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>thirdweb NFT Drop Minting Customizable Page</title>
+        <title>DD Test</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
